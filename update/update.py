32919d870a87fa5750ad440694fd730592bf282d
Thomas Glauser <thomas@glauser.dev>
2020-06-23 17:57:56 +0200

    GPG signature check on auto updates

--- update/update.py
+++ update/update.py
@@ -1,9 +1,24 @@
 import os
+import fs
+from fs import open_fs
+import gnupg
 
-os.system('cd /home/pi/relay-control/app && rm -rf *')
+gpg = gnupg.GPG()
 
-os.system('cd /home/pi/ && wget https://github.com/AUThomasCH/relay-control/releases/latest/download/relay-control.zip && unzip -o relay-control && rm relay-control.zip')
+os.system('cd /home/pi/ && wget https://github.com/AUThomasCH/relay-control/releases/latest/download/relay-control.zip')
 
-os.system('service relay-control-app start')
+fileStream = open('/home/pi/relay-control.zip', "rb")
+signature = gpg.verify_file(fileStream)
+
+if signature.status == "signature valid":
+    print("Signature is valid!")
+    os.system('rm -rf /home/pi/relay-control/app/*')
+    os.system('unzip /home/pi/relay-control.zip')
+    os.system('rm /home/pi/relay-control.zip')
+    os.system('service relay-control-app start')
+else:
+    print("Signature invalid or missing!")
+    print("Aborting script execution")
+
 
 raise SystemExit
