370b519eb7049e098d3a3db4373fe8d423a6abb3
Thomas Glauser <thomas@glauser.dev>
2020-06-23 18:17:21 +0200

    GPG update decrypt bugfix

--- update/update.py
+++ update/update.py
@@ -5,15 +5,24 @@ import gnupg
 
 gpg = gnupg.GPG()
 
-os.system('cd /home/pi/ && wget https://github.com/AUThomasCH/relay-control/releases/latest/download/relay-control.zip')
+os.system('cd /home/pi/ && wget https://github.com/AUThomasCH/relay-control/releases/latest/download/relay-control.zip.gpg')
 
-fileStream = open('/home/pi/relay-control.zip', "rb")
+fileStream = open('/home/pi/relay-control.zip.gpg', "rb")
 signature = gpg.verify_file(fileStream)
 
 if signature.status == "signature valid":
     print("Signature is valid!")
+
+    with open('/home/pi/relay-control.zip.gpg', 'rb') as f:
+        status = gpg.decrypt_file(
+            file=f,
+            output='/home/pi/relay-control.zip',
+    )
+
+    print(status.stderr)
+
     os.system('rm -rf /home/pi/relay-control/app/*')
-    os.system('unzip /home/pi/relay-control.zip')
+    os.system('unzip -o /home/pi/relay-control.zip')
     os.system('rm /home/pi/relay-control.zip')
     os.system('service relay-control-app start')
 else:
